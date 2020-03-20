41b2999c93e932e7bdf518f634b41bd668d547cd
Grayzer <abris80@gmail.com>
2020-03-20 23:40:38 +0300

    half works

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,53 +24,192 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.Process1 = New System.Diagnostics.Process()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel3 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel4 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
         Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'TabControl1
         '
         Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
         Me.TabControl1.Location = New System.Drawing.Point(12, 12)
         Me.TabControl1.Name = "TabControl1"
         Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(776, 426)
+        Me.TabControl1.Size = New System.Drawing.Size(1007, 527)
         Me.TabControl1.TabIndex = 0
         '
         'TabPage1
         '
+        Me.TabPage1.Controls.Add(Me.ListBox2)
+        Me.TabPage1.Controls.Add(Me.TextBox4)
+        Me.TabPage1.Controls.Add(Me.TextBox3)
+        Me.TabPage1.Controls.Add(Me.TextBox2)
+        Me.TabPage1.Controls.Add(Me.TextBox1)
+        Me.TabPage1.Controls.Add(Me.ComboBox1)
+        Me.TabPage1.Controls.Add(Me.ListBox1)
         Me.TabPage1.Location = New System.Drawing.Point(4, 22)
         Me.TabPage1.Name = "TabPage1"
         Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(768, 400)
+        Me.TabPage1.Size = New System.Drawing.Size(999, 501)
         Me.TabPage1.TabIndex = 0
-        Me.TabPage1.Text = "BackUp"
+        Me.TabPage1.Text = "Language"
         Me.TabPage1.UseVisualStyleBackColor = True
         '
-        'Process1
+        'TextBox3
         '
-        Me.Process1.StartInfo.Domain = ""
-        Me.Process1.StartInfo.LoadUserProfile = False
-        Me.Process1.StartInfo.Password = Nothing
-        Me.Process1.StartInfo.StandardErrorEncoding = Nothing
-        Me.Process1.StartInfo.StandardOutputEncoding = Nothing
-        Me.Process1.StartInfo.UserName = ""
-        Me.Process1.SynchronizingObject = Me
+        Me.TextBox3.Location = New System.Drawing.Point(298, 59)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(674, 20)
+        Me.TextBox3.TabIndex = 14
+        Me.TextBox3.Text = "Steam-код"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(298, 33)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(674, 20)
+        Me.TextBox2.TabIndex = 13
+        Me.TextBox2.Text = "Full Path Selected Mod"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(298, 7)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(674, 20)
+        Me.TextBox1.TabIndex = 12
+        Me.TextBox1.Text = "Selected Name Mod"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(6, 6)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(263, 21)
+        Me.ComboBox1.TabIndex = 11
+        Me.ComboBox1.Text = "Язык"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {"Список модов"})
+        Me.ListBox1.Location = New System.Drawing.Point(6, 36)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.ScrollAlwaysVisible = True
+        Me.ListBox1.Size = New System.Drawing.Size(263, 446)
+        Me.ListBox1.TabIndex = 7
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Size = New System.Drawing.Size(999, 501)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Backup"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripStatusLabel2, Me.ToolStripStatusLabel3, Me.ToolStripStatusLabel4})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 542)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(1031, 22)
+        Me.StatusStrip1.TabIndex = 6
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(40, 17)
+        Me.ToolStripStatusLabel1.Text = "Steam"
+        '
+        'ToolStripStatusLabel2
+        '
+        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(58, 17)
+        Me.ToolStripStatusLabel2.Text = "Rimworld"
+        '
+        'ToolStripStatusLabel3
+        '
+        Me.ToolStripStatusLabel3.Name = "ToolStripStatusLabel3"
+        Me.ToolStripStatusLabel3.Size = New System.Drawing.Size(77, 17)
+        Me.ToolStripStatusLabel3.Text = "Версия игры"
+        '
+        'ToolStripStatusLabel4
+        '
+        Me.ToolStripStatusLabel4.Name = "ToolStripStatusLabel4"
+        Me.ToolStripStatusLabel4.Size = New System.Drawing.Size(95, 17)
+        Me.ToolStripStatusLabel4.Text = "Готово к работе"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(298, 85)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.Size = New System.Drawing.Size(674, 20)
+        Me.TextBox4.TabIndex = 15
+        Me.TextBox4.Text = "Найден ли перевод в базе"
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Items.AddRange(New Object() {"Список найденных языков"})
+        Me.ListBox2.Location = New System.Drawing.Point(298, 111)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.ScrollAlwaysVisible = True
+        Me.ListBox2.Size = New System.Drawing.Size(270, 368)
+        Me.ListBox2.TabIndex = 16
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1031, 564)
+        Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.Text = "Rimworld Mod Utility"
         Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents TabControl1 As TabControl
     Friend WithEvents TabPage1 As TabPage
-    Friend WithEvents Process1 As Process
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel2 As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel3 As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel4 As ToolStripStatusLabel
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents TextBox4 As TextBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,283 @@
 ﻿Public Class Form1
+    Friend SteamLib As String = ""
+    Friend SteamEnable As Boolean = False
+    Friend PathSteam As String = ""
+    Friend SteamRimEnable As Boolean = False
+    Friend RimEnable As Boolean = False
+    Friend PathSteamRim As String = ""
+    Friend PathRim As String = ""
+    Friend SteamRimWorkshopEnable As Boolean = False
+    Friend PathSteamRimWorkshop As String = ""
+    Friend PathLang As String = ""
+    Friend PathMod As String = ""
+    Friend RimSteamMod As String()
+    Friend RimLocMod As String()
+    Friend RimSteamModFullPath As String()
+    Friend RimLocModFullPath As String()
+    Friend RimSteamModName As String()
+    Friend RimLocModName As String()
+    Friend ModNames() As (String, String, String)
+    Friend CountSteamMods As Integer = 0
+    Friend CountWorkMods As Integer = 0
+    Friend PathDataDir As String = "f:\GitHub\Rim Utility\Data\"
+
+    Public Function CheckDataDir(_modID As String) As Boolean
+        If IO.Directory.Exists(PathDataDir + _modID) Then
+            TextBox4.Text = "Данный мод присутствует в базе"
+            Return (True)
+        Else
+            TextBox4.Text = "Данный мод отсутствует в базе"
+            ListBox2.Items.Clear()
+            Return (False)
+        End If
+
+    End Function
+
+    Public Function GetDataLang(_modID) As Boolean
+        Dim s As String = PathDataDir + _modID + "\Languages"
+        If IO.Directory.Exists(s) Then
+            Dim lll As Integer = s.Length
+            Dim ddd = IO.Directory.GetDirectories(s)
+            Dim xxx = ddd.Count
+            For i = 0 To xxx - 1
+                If ddd.ElementAt(i).Length > lll Then
+                    ddd(i) = ddd.ElementAt(i).Substring(lll + 1)
+                    'ddd(i) = Mid(ddd.ElementAt(i), 1, Len(ddd.ElementAt(i)) - 4).ToString
+                End If
+            Next
+
+            ListBox2.Items.Clear()
+            ListBox2.Items.AddRange(ddd)
+            If xxx > 0 Then ListBox2.SelectedIndex = 0
+        End If
+        If ListBox2.Items.Count > 0 Then Return (True) Else Return (False)
+
+    End Function
+
+    Public Function getmods() As Boolean
+        TextBox1.Text = ModNames(ListBox1.SelectedIndex).Item1
+        TextBox2.Text = ModNames(ListBox1.SelectedIndex).Item2
+        TextBox3.Text = ModNames(ListBox1.SelectedIndex).Item3
+        If CheckDataDir(ModNames(ListBox1.SelectedIndex).Item3) Then
+            GetDataLang(ModNames(ListBox1.SelectedIndex).Item3)
+        End If
+        Return (True)
+    End Function
+    Public Function SortMods() As Boolean
+        'Dim _item1 As String   'сортируем список модов методом пузырька
+        'Dim _item2 As String
+        'Dim _item3 As String
+        'For i = 0 To CountSteamMods + CountWorkMods - 1
+        '    For z = i + 1 To CountSteamMods + CountWorkMods - 1
+        '        If ModNames(z).Item1 < ModNames(i).Item1 Then
+        '            _item1 = ModNames(i).Item1
+        '            _item2 = ModNames(i).Item2
+        '            _item3 = ModNames(i).Item3
+        '            ModNames(i).Item1 = ModNames(z).Item1
+        '            ModNames(i).Item2 = ModNames(z).Item2
+        '            ModNames(i).Item3 = ModNames(z).Item3
+        '            ModNames(z).Item1 = _item1
+        '            ModNames(z).Item2 = _item2
+        '            ModNames(z).Item3 = _item3
+        '        End If
+        '    Next
+        'Next
+        Dim z = ModNames.OrderBy(Function(s) s.Item1)   'а это вариант сортировки через ордер. но пузырьком красивее...
+        ModNames = z.ToArray
+
+
+        Return (True)
+    End Function
+    Public Function RimLang() As Boolean
+
+        If SteamRimEnable Then PathLang = PathSteamRim + "\Data\Core\Languages\"
+        If RimEnable Then PathLang = PathRim + "\Data\Core\Languages\"
+        Dim lll As Integer = PathLang.Length
+        Dim ddd = IO.Directory.GetFiles(PathLang)
+        Dim xxx = ddd.Count
+        If xxx > 0 Then
+            For i = 0 To xxx - 1
+                If ddd.ElementAt(i).Length > lll Then
+                    ddd(i) = ddd.ElementAt(i).Substring(lll)
+                    If ddd.ElementAt(i).ToLower <> "english" Then ddd(i) = Mid(ddd.ElementAt(i), 1, Len(ddd.ElementAt(i)) - 4).ToString
+                End If
+            Next
+        End If
+        If SteamRimEnable Or RimEnable Then
+            ComboBox1.Items.Clear()
+            ComboBox1.Items.Add("English")
+            ComboBox1.Items.AddRange(ddd)
+            ComboBox1.SelectedIndex = 0
+            Return (True)
+        Else
+            ComboBox1.Items.Clear()
+            ComboBox1.Items.Add("English")
+            ComboBox1.SelectedIndex = 0
+            Return (False)
+        End If
+
+        Return (0)
+    End Function
+
+    Public Function NameMod(FullPathMod As String) As String
+        Dim ModName As String = "unknow"
+        If IO.Directory.Exists(FullPathMod + "\About") Then
+            If IO.File.Exists(FullPathMod + "\About\About.xml") Then
+                Dim doc As XDocument = XDocument.Load(FullPathMod + "\About\About.xml")
+                ModName = doc.Root.Element("name").Value.ToString
+            End If
+        End If
+
+        Return (ModName)
+    End Function
+
+    Public Function GetID(_path As String) As String
+        Dim PFD As String = vbEmpty
+        If IO.File.Exists(_path + "\About\PublishedFileId.txt") Then PFD = IO.File.ReadAllText(_path + "\About\PublishedFileId.txt")
+        Return (PFD)
+    End Function
+
+    Public Function FillNameMod() As Boolean
+
+        If SteamRimEnable Then
+            Dim ListMods = My.Computer.FileSystem.GetDirectories(PathSteamRim + "\Mods")
+            Dim WListMods = My.Computer.FileSystem.GetDirectories(PathSteamRimWorkshop)
+            Dim SteamID As String = vbEmpty
+            CountSteamMods = ListMods.Count
+            CountWorkMods = WListMods.Count
+            ReDim ModNames(CountSteamMods + CountWorkMods - 1)
+            ListBox1.Items.Clear()
+            If CountSteamMods > 0 Then
+                For i = 0 To CountSteamMods - 1
+                    ModNames(i).Item1 = NameMod(ListMods(i))
+                    ModNames(i).Item2 = ListMods(i)
+                    ModNames(i).Item3 = GetID(ModNames(i).Item2)
+                Next
+            End If
+            If CountWorkMods > 0 Then
+                For i = 0 To CountWorkMods - 1
+                    ModNames(i + CountSteamMods).Item1 = NameMod(WListMods(i))
+                    ModNames(i + CountSteamMods).Item2 = WListMods(i)
+                    ModNames(i + CountSteamMods).Item3 = GetID(ModNames(i + CountSteamMods).Item2)
+                Next
+            End If
+        End If
+        If CountSteamMods + CountWorkMods > 0 Then
+            SortMods()
+            For i = 0 To CountSteamMods + CountWorkMods - 1
+                ListBox1.Items.Add(ModNames(i).Item1.ToString)
+            Next
+        End If
+
+        Return (True)
+    End Function
+
+    Public Function FillModList() As Boolean
+        Dim lll As Integer = 0
+        Dim CurModPath As String = ""
+
+        If SteamRimEnable Then CurModPath = PathSteamRim + "\Mods"
+        If RimEnable Then CurModPath = PathRim + "\Mods"
+        lll = CurModPath.Length
+        Dim aaa = IO.Directory.GetDirectories(CurModPath)
+        Dim xxx = aaa.Count
+        Dim zzz As String = ""
+
+        If SteamRimEnable Then
+            RimSteamModFullPath = IO.Directory.GetDirectories(CurModPath)
+            RimSteamMod = aaa
+        End If
+
+        If RimEnable Then
+            RimLocModFullPath = IO.Directory.GetDirectories(CurModPath)
+            RimLocMod = aaa
+        End If
+
+        For i = 0 To xxx - 1
+            If SteamRimEnable Then
+                RimSteamMod(i) = aaa(i).Substring(lll + 1)
+            End If
+
+            If RimEnable Then
+                RimLocMod(i) = aaa(i).Substring(lll + 1)
+            End If
+
+        Next
+
+        If SteamRimEnable Then
+            ListBox1.Items.Clear()
+            FillNameMod()
+        Else
+            ListBox1.Items.Clear()
+        End If
+
+        ListBox1.Refresh()
+
+        Return (True)
+    End Function
+
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim aaa As String = ""
+        Dim bbb As Integer = 0
+
+        If My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\Valve\Steam", "SteamPath", Nothing) Is Nothing Then
+            SteamEnable = False
+            ToolStripStatusLabel1.Text = "Steam: не найден."
+        Else
+            SteamEnable = True
+            PathSteam = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\Valve\Steam", "SteamPath", Nothing)
+            ToolStripStatusLabel1.Text = "Steam: установлен."
+        End If
+
+        If My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\Valve\Steam\Apps\294100", "Installed", Nothing) Is Nothing Then
+            SteamRimEnable = False
+            ToolStripStatusLabel2.Text = "Rimword: не найден."
+        Else
+            If My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\Valve\Steam\Apps\294100", "Installed", Nothing) = 1 Then
+                SteamRimEnable = True
+                ToolStripStatusLabel2.Text = "Rimword: установлен."
+
+                Dim zzz = My.Computer.FileSystem.ReadAllText(PathSteam + "\config\config.vdf")
+
+                bbb = InStr(zzz, "BaseInstallFolder_1")
+                If bbb > 0 Then
+                    aaa = Trim(zzz.Substring(bbb + 22))
+                    bbb = InStr(aaa, """")
+                    aaa = Mid(aaa, 1, bbb - 1)
+                    While InStr(aaa, "\\")
+                        bbb = InStr(aaa, "\\")
+                        aaa = aaa.Remove(bbb, 1)
+                    End While
+                    PathSteamRim = aaa + "\SteamApps\common\RimWorld"
+                    PathSteamRimWorkshop = aaa + "\SteamApps\workshop\content\294100"
+
+                    If IO.File.Exists(PathSteamRim + "\Version.txt") Then
+                        Dim vvv As String = My.Computer.FileSystem.ReadAllText(PathSteamRim + "\Version.txt")
+                        vvv = vvv.Trim(vbCr, vbLf)
+                        ToolStripStatusLabel3.Text = "Версия игры: " + vvv
+                    Else
+                        ToolStripStatusLabel3.Text = "Версия игры: неизвестна"
+                    End If
+
+
+
+                End If
+
+
+            End If
+
+
+        End If
+        RimLang()
+        FillModList()
+        ListBox1.SelectedIndex = 0
+        getmods()
 
     End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        getmods()
+    End Sub
+
 End Class
